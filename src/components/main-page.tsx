c5f9ee14247c56c4fc008ba03a509c9c18276581
kambekman <artemapiyn2002@gmail.com>
2025-05-10 01:54:59 +0300

    Main-page

--- src/components/main-page.tsx
+++ src/components/main-page.tsx
@@ -3,11 +3,11 @@ import { TitleMainPage } from "./title-main-page"
 
 const MainPage = React.memo(() => {
     return (
-        <div className="will-change-transform relative min-h-[100vh] overflow-hidden flex items-center px-5 md:px-20">
+        <div className="will-change-transform relative min-h-[100vh] overflow-hidden flex items-center px-5 py-1 md:px-20">
         <div id='main' className="absolute -top-[250px] " aria-hidden="true"></div>
         <div className="absolute inset-0 z-0 ">
             <div
-            className="w-auto h-full bg-scroll md:bg-fixed"
+            className="w-auto h-full bg-scroll lg:bg-fixed"
             style={{
                 backgroundImage: `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.6)), url("/main-page.webp")`,
                 backgroundSize: "cover",
