b8ea8440a79b9df36acd3b74f497bacd0dec5944
artemapian <artemapiyn2002@gmail.com>
2025-05-09 23:40:50 +0300

    Update route.ts

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
+const resend = new Resend("re_YkiqWhqV_GEpvo6XnRtMNMEVFUazEQsY1");
 
 export async function POST(req: Request) {
   try {
