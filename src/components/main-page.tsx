8d14432c9863c9441e9f5d46af5a964024be38e7
kambekman <artemapiyn2002@gmail.com>
2025-05-10 21:08:52 +0300

    main-page

--- src/components/main-page.tsx
+++ src/components/main-page.tsx
@@ -3,7 +3,7 @@ import { TitleMainPage } from "./title-main-page"
 
 const MainPage = React.memo(() => {
     return (
-        <div className="relative h-[100svh] overflow-hidden flex items-center px-5 py-1 md:px-20">
+        <div className="relative title-main-page min-h-[100vh] overflow-hidden flex items-center px-5 py-1 md:px-20">
         <div id='main' className="absolute -top-[250px] " aria-hidden="true"></div>
         <div className="absolute inset-0 z-0 ">
             <div
