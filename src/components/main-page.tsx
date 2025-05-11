6f91d856554ff009a848e250be5550b908922ed4
kambekman <artemapiyn2002@gmail.com>
2025-05-11 16:49:50 +0300

    new Main-page

--- src/components/main-page.tsx
+++ src/components/main-page.tsx
@@ -3,11 +3,11 @@ import { TitleMainPage } from "./title-main-page"
 
 const MainPage = React.memo(() => {
     return (
-        <div className="relative h-[100lvh] overflow-hidden flex items-center px-5 py-1 md:px-20">
+        <div className="main-page relative mt-[50px] md:mt-0 max-h-[100svh] md:max-h-[100lvh] md:h-[100lvh] overflow-hidden flex items-center px-5 py-1 md:px-20">
         <div id='main' className="absolute -top-[250px] " aria-hidden="true"></div>
         <div className="absolute inset-0 z-0 ">
             <div
-            className="w-auto h-[100dvh] max-h-100% bg-scroll lg:bg-fixed bg-cover bg-center bg-no-repeat"
+            className="w-auto h-full bg-scroll lg:bg-fixed bg-cover bg-center bg-no-repeat"
             style={{
                 backgroundImage: `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.6)), url("/main-page.webp")`
             }}
--- src/components/title-main-page.tsx
+++ src/components/title-main-page.tsx
@@ -16,13 +16,13 @@ export const TitleMainPage = () => {
         <h1 className="font-bold">
             Аренда спецтехники в Саратове и Саратовской области
         </h1>
-        <p className="main-page text-lg md:text-xl mt-2 sm:mt-4">
+        <p className="main-page text-[16px] md:text-xl mt-2 sm:mt-4">
             Предоставляем услуги аренды строительной и грузовой техники с опытными операторами и доставку строительных материалов. Работаем с объектами любой сложности по всему Саратову и Энгельсу.
         </p>
         <div className="2xl:my-8 my-4 space-x-5 flex flex-wrap md:space-y-0 space-y-4">
             <Application 
             title="Заказать технику"
-            className="bg-[#ec9f23] outline-none cursor-pointer  hover:bg-[#eba83c]  text-white md:text-xl flex w-full h-[48px] px-7  md:w-auto justify-center text-lg font-semibold"
+            className="bg-[#ec9f23] outline-none cursor-pointer  hover:bg-[#eba83c]  text-white md:text-xl flex w-full h-[46px] px-7  md:w-auto justify-center text-lg font-semibold"
             />
             <Button
                 variant='link'
@@ -32,7 +32,7 @@ export const TitleMainPage = () => {
                     element.scrollIntoView({ behavior: "smooth" });
                     }
                 }} 
-                className="hover:text-[#ec9f23] flex md:w-auto w-full h-[48px] justify-center px-7 items-center rounded-md text-white border border-white hover:border-[#ec9f23] md:text-xl text-lg font-semibold hover:bg-transparent">
+                className="hover:text-[#ec9f23] flex md:w-auto w-full h-[46px] justify-center px-7 items-center rounded-md text-white border border-white hover:border-[#ec9f23] md:text-xl text-lg font-semibold hover:bg-transparent">
                 Посмотреть каталог
             </Button>
         </div>
