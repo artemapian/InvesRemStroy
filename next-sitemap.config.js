e17c96a152025ca378435734e738aed7111b9d9a
kambekman <artemapiyn2002@gmail.com>
2025-05-31 14:43:38 +0300

    site-map

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://spec-tehn.ru', // Заменить на свой домен
+  generateRobotsTxt: true,         // Автоматически сгенерирует robots.txt
+  sitemapSize: 5000,
+};
